c0da7b17055327f3f6243bff1d674bf610278716
Versoiu Tudor <versoiutudorsorin@gmail.com>
2021-01-12 15:58:49 +0200

    Upload all

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: 'user-list.page.html',
+  styleUrls: ['user-list.page.scss']
+})
+export class UserListPage {
+
+  constructor() {}
+
+}
